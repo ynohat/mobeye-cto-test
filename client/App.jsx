717eb9cf3530712614b2b284229e292b5c91ded6
Anthony Hogg <anthony@hogg.fr>
2016-03-07 00:40:35 +0100

    add CSV download logic

--- client/App.jsx
+++ client/App.jsx
@@ -1,7 +1,13 @@
-// Root of the frontend display tree.
-//
-// Note: we need to assign to App so it's available in the global scope,
-// see http://blog.east5th.co/2015/09/23/exporting-es6-classes-from-meteor-packages/
+/**
+ * Root of the frontend display tree.
+ *
+ * For this simple exercise, this component also acts as the controller and
+ * handles all the business logic (as opposed to putting it in Flux actions or
+ * some other more Meteor-ish... thing).
+ *
+ * Note: we need to assign to App so it's available in the global scope,
+ * see http://blog.east5th.co/2015/09/23/exporting-es6-classes-from-meteor-packages/
+ */
 App = class App extends React.Component {
     constructor(props) {
         super(props);
@@ -37,6 +43,28 @@ App = class App extends React.Component {
     }
 
     onStoreSelectionDownload() {
-        alert("todo");
+        // This works, using the clinical:csv meteor package, and does not
+        // require any work server-side, but it isn't ideal:
+        // 
+        // - the window.open() method does not allow a filename to be specified,
+        // so the CSV file gets downloaded as "download", "download (1)" and so
+        // forth; without a file extension, the CSV file is hardly usable by a
+        // lambda user; solving this problem without actually storing the file
+        // on the server requires creating an anchor link with download attribute
+        // which is not cross browser compatible
+        // 
+        // - with regard to the tech test parameters, this is clearly cheating,
+        // because no server side logic is involved :)
+        //
+        // It will do.
+        var csvContent = CSV.unparse(this.state.selection.map((store) => {
+            return {
+                name: store.name,
+                address: store.formatted_address,
+                lat: store.geometry.location.lat,
+                lng: store.geometry.location.lng
+            };
+        }));
+        window.open('data:text/csv;charset=utf-8,' + escape(csvContent), '_self');
     }
 }
